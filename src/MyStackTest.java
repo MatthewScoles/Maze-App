3ebfa405d6035f19cc34af6eb83fa10a0974c0a0
lakshyaanathan <lknathan@stu.naperville203.org>
2023-11-06 19:01:59 -0600

    Minor change with object attributes

--- src/MyStackTest.java
+++ src/MyStackTest.java
@@ -1,8 +1,8 @@
 public class MyStackTest {
     public static void main(String [] args){
         MyStack stack = new MyStack();
-        Square square = new Square(1, 2, "one");
-        Square square2 = new Square(2, 3, "one");
+        Square square = new Square(1, 2, "one", square2);
+        Square square2 = new Square(2, 3, "one", square);
 
         stack.push(square);
         stack.push(square2);
