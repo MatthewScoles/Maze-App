f7a36d079ddc52fa32749fbb100b0b6425c44999
lakshyaanathan <lknathan@stu.naperville203.org>
2023-11-27 16:31:34 -0600

    Overriding changes

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,68 +1,68 @@
-import java.util.*;
+
+import java.util.NoSuchElementException;
+
+class Node{
+    
+    Square data;
+    Node next;
+     Node(Square content){
+        this.data = content;
+        this.next = null;
+    }
+}
+
 
 public class MyStack<T> implements StackADT
 
 {
-    private ArrayList list;
-    T object;
+    private Node node;
+
+    private int len;
 
     public MyStack(){
-      list = new ArrayList<>();
+      len = 0;
+
+      node = null;
       
     }
 
-     public void push(Object T){
-        list.add(object);
+     public void push(Object el){
+      Node newNode = new Node();
+      newNode.data = el;
+      newNode.next = node;
+      this.node = newNode;
+      len++;
+
+
+
      }
 
-     public T pop() throws NoSuchElementException{
-         if(!isEmpty()){
-         try{
-            return (T) (list.remove(list.size()-1));
-            
-         }
-         catch(NoSuchElementException el){
-            System.out.println("This is not an element");
-            return null;
-         }}
-         else 
-            return null;
+     public Object pop(){
+      if (this.isEmpty())
+      {
+          throw new NoSuchElementException();
+      }
+      Object element = this.node.data;
+      this.node = this.node.next;
+      len--;
+      return element;
      }
 
-     public Square top() throws NoSuchElementException{
-        if(!isEmpty()){
-         try{
-            return (Square) (list.get(0));
-         }
-         catch (NoSuchElementException el){
-            System.out.println("This is not an element");
-            return null;
-         }}
-      else
-         return null;
+     public Object top(){
+        return this.node.data;
      }
 
      public int size(){
-      return list.size();
+      return len;
      }
 
       public boolean isEmpty(){
-         if(list.size()==0){
-            return true;
-         }
-         else{
-            return false;
-         }
+         return this.node==null;
       }
 
       public void clear(){
-         for(int i = 0; i<list.size(); i++){
-            list.remove(i);
-         }
+        while(!(this.node==null)){
+         pop();
+        }
       }
-
-      
-    
-
-     
 }
